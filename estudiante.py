6a6fd880376bbab735d17294e534ba0d7cd2229b
DanielBerruezo-upct-es <daniel.berruezo@edu.upct.es>
2024-04-05 11:22:25 +0200

    Segunda versión de estudiante

--- estudiante.py
+++ estudiante.py
@@ -6,6 +6,12 @@ class Estudiante(Persona):
         super().__init__(nombre, dni, direccion, sexo)
         self.asignaturas = set()
 
+    def __str__(self):
+        a = ''
+        for asign in self.asignaturas:
+            a += asign.obtener_nombre() + ' '   
+        return self.obtener_nombre() + ' Asignaturas: ' + a
+
     def obtener_asignaturas(self):
         a = ''
         for asign in self.asignaturas:
@@ -14,21 +20,16 @@ class Estudiante(Persona):
 
     def añadir_asignatura(self, nueva_asignatura:Asignatura):
         """El estudiante escoge la asignatura a matricularse"""
-        try:
-            if not isinstance(nueva_asignatura, Asignatura):
-                raise TypeError("El argumento 'nueva_asignatura' debe ser una instancia de la clase Asignatura")
-            self.asignaturas.add(nueva_asignatura)
 
-        except TypeError as e:
-            print("Error:", e)
+        if not isinstance(nueva_asignatura, Asignatura):
+            raise TypeError("El argumento 'nueva_asignatura' debe ser una instancia de la clase Asignatura")
+        self.asignaturas.add(nueva_asignatura)
 
     def asignatura_aprobada(self, asignatura:Asignatura):
-        try:
-            if not isinstance(asignatura, Asignatura):
-                raise TypeError("El argumento 'asignatura' debe ser una instancia de la clase Asignatura")
-            if asignatura not in self.asignaturas:
-                raise ValueError("El argumento 'asignatura' debe estar en la lista de asignaturas del estudiante")
-            self.asignaturas.remove(asignatura)
 
-        except (TypeError, ValueError) as e:
-            print("Error:", e)
+        if not isinstance(asignatura, Asignatura):
+            raise TypeError("El argumento 'asignatura' debe ser una instancia de la clase Asignatura")
+        if asignatura not in self.asignaturas:
+            raise ValueError("El argumento 'asignatura' debe estar en la lista de asignaturas del estudiante")
+        
+        self.asignaturas.remove(asignatura)
